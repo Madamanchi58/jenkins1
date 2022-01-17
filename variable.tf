524f91cec33889cec04cb17dea3c56c82d435c93
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-01-17 18:24:56 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,8 +4,14 @@ default="ami-08e4e35cccc6189f4"
 variable type{
 default="t2.micro"
 }
-variable name{
+variable ec2name{
 default="My-Ec2"
+}
+variable s3name{
+  default="s3"
+}
+variable env{
+  default="dev"
 }
 variable region{
   default="us-east-1"
