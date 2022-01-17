cd1cb1b0a9feae1fc654bd014d8360be30b8f9e9
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-01-17 20:52:15 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,9 +2,9 @@ provider "aws"{
   region="us-east-1"
 }
 resource "aws_s3_bucket" "black" {
-  bucket ="MY_New_bucket"
+  bucket ="my-tf-test-bucket"
   tags = {
     Environment = var.env
-    name=var.name
+    Name=var.name
   }
 }
