031b35881e4a6c2e361391787489c7e88f1b76c1
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-01-18 18:46:12 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,17 @@
 provider "aws"{
-  region="us-west-2"
+  region="us-west-1"
 }
+provider "aws"{
+  region=var.region
+  }
+resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.type
+  tags={
+    Name=var.ename
+  }
+}
+
 resource "aws_s3_bucket" "_abc" {
   bucket =var.name
   tags = {
