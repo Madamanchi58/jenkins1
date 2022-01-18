fe39cd875ddacef75b2195fc231dcda561cfa8d7
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-01-18 09:53:48 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,8 +2,9 @@ provider "aws"{
   region="us-east-1"
 }
 resource "aws_s3_bucket" "b" {
-  bucket =var.bname
+  bucket ="MY-NEW_BCUCKET"
   tags = {
     Environment = var.env
+    Name=var.bname
   }
 }
