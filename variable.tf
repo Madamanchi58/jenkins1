c902814b1409fbf5c12e8a8df4b336465cc84c10
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-01-18 18:45:22 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,3 +4,15 @@ variable name{
 variable env{
 default="dev"
 }
+variable ami{
+default="ami-08e4e35cccc6189f4"
+}
+variable type{
+default="t2.micro"
+}
+variable ename{
+default="My-Ec2"
+}
+variable region{
+  default="us-east-1"
+}
