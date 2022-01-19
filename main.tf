f1634a4f028652cd0cf194afb0f790cba7b23951
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-01-19 16:51:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws"{
   region=var.region
   }
 resource "aws_instance" "web" {
-  ami           = "$lookup(var.ami,var.name)}"
+  ami           = "$lookup(var.name,var.ami)}"
   instance_type = var.type
   tags={
     Name=var.name
