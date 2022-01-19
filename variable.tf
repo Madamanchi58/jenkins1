e67e6022f2be047f510000ab32d2f538808ae1fb
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-01-19 16:18:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,20 +1,18 @@
-variable ami{
-default="ami-08e4e35cccc6189f4"
-}
-variable w{
-  default=""
-}
-variable l{
-  default="ami-08e4e35cccc6189f4"
-}
 variable type{
 default="t2.micro"
 }
-variable ename{
+variable name{
 default="My-Ec2"
 }
 variable region{
   default="us-east-1"
+}
+variable "ami"{
+  type="map"
+  default={
+    windows=""
+    linux=""
+  }
 }
 
 
