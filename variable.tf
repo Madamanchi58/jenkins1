a729d1ff41f53daab56b7264cc9382ef54320443
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-01-19 19:36:47 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,18 +1,14 @@
-variable type{
-default="t2.micro"
+variable "region" {
+  default = "us-east-1"
 }
-variable name{
-default="linux"
-}
-variable region{
-  default="us-east-1"
-}
-variable "ami"{
-  type=map
-  default={
-    "linux"="ami-08e4e35cccc6189f4"
-    "windows"="ami-0d43d465e2051057f"
+variable "ami" {
+  default = {
+    "linux"   = "ami-08e4e35cccc6189f4"
+    "windows" = "ami-0d43d465e2051057f"
   }
+}
+variable "name" {
+  default = "windows"
 }
 
 
