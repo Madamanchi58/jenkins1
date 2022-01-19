6fe74e1227a41c709752eeeb2f9b852aa4e5e78a
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-01-19 17:14:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws"{
   region=var.region
   }
 resource "aws_instance" "web" {
-  foreach="${var.ami}"
-  ami           = "$lookup(each.value,"var.name",NULL)"
+  foreach = "${var.ami}"
+  ami           = lookup(each.value,"var.name",NULL)
   instance_type = var.type
   tags={
     Name=var.name
