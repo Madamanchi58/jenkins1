fd2a6724eade8ab850bbbecb4d4ea03c6251e2db
Madamanchi58 <59084300+Madamanchi58@users.noreply.github.com>
2022-02-03 17:15:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@ region=var.region
 resource "aws_instance" "lec2"{
 ami=var.ami
 instance_type=var.type
+ user_data = <<-EOF
+ #!/bin/bash
+ echo "*** Installing apache2"
+ sudo apt update -y
+ sudo apt install apache2 -y
+ echo "*** Completed Installing apache2"
+ EOF
 tags={
 Name=var.name
 }
